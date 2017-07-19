4feb32c6deaaedc5ebcce2dc623c8e2ed5bf364d
Xiaoxi <pluto.xiaoxi@gmail.com>
2017-07-19 21:59:52 +1000

    ann_1

--- ann/chap1.py
+++ ann/chap1.py
@@ -6,3 +6,19 @@ Created on Wed Jul 19 21:34:28 2017
 """
 
 # http://neuralnetworksanddeeplearning.com/chap1.html
+
+import numpy as np
+
+class Network:    
+    def __int__(self, sizes):
+        # sizes contains the number of neurons
+        self.num_layers = len(sizes)
+        self.sizes = sizes
+        self.biases = [np.random.randn(y,1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y,x) for x,y in zip(sizes[:-1], sizes[1:])]
+         
+        
+                                
+                                
+                            
+        
